100d627c7a2666d772634dd09c9ea282026f7b20
yuhjak25 <yuhjakcontact@gmail.com>
2025-03-04 09:29:49 +0100

    add logging middleware to track request duration and status

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,9 @@ import express from 'express'
 export const app = express()
 
 import { linkRoutes } from './routes/link'
+import { logMiddleware } from './middleware/logMiddleware'
 
-app.use(express.json())
+app
+    .use(express.json())
+    .use(logMiddleware)
     .use('/api/links', linkRoutes)
