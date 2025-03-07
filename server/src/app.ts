35d4990e97d054c8afe7e1863be3f982460f1a0b
yuhjak25 <yuhjakcontact@gmail.com>
2025-03-07 23:29:21 +0100

    feat: add request method logging middleware for performance tracking

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import linkRoutes from './routes/link-rou'
+import methodLogger from './middleware/methodsLogger'
 
 const app = express()
 
 app
     .use(express.json())
     .use(cors())
+    .use(methodLogger)
     .use('/api/links', linkRoutes)
 
 export default app
---
+++ server/src/middleware/methodsLogger.ts
@@ -0,0 +1,16 @@
+import type { Request, Response, NextFunction } from "express";
+
+const methodLogger = (req: Request, res: Response, next: NextFunction) => {
+    const startTime = Date.now()
+
+    res.on("finish", () => {
+        const duration = Date.now() - startTime
+        console.log(
+            `${req.method} - ${req.path} ${res.statusCode} ${duration}ms`
+        )
+    })
+
+    next()
+}
+
+export default methodLogger
