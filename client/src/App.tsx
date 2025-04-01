93d050ab3a1674d5284eecf87a64a2248fe5c8d9
yuhjak25 <yuhjakcontact@gmail.com>
2025-04-01 11:34:24 +0200

    feat: increase header font size for improved visibility in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import LinkList from './pages/Links/LinkList'
 function App() {
   return (
     <div className='h-screen flex flex-col items-center pt-50 bg-eerie'>
-      <h1 className='text-white text-2xl font-semibold p-4'>SquishLink</h1>
+      <h1 className='text-white text-4xl font-semibold p-4'>SquishLink</h1>
 
       <LinkList />
     </div>
